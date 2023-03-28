c92081834c086e7bd4de29fc0dd5dcb240392e2a
PrinceM <parames.pasitprachaja@gmail.com>
2023-03-28 10:19:08 +0700

    Revert "fix(google sheet): connect and fetch data directly from google sheet in server component (previously did in api)"
    This reverts commit 51f5dac066ea11ca94e33c2407ba06c906fa1683.

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,38 +1,14 @@
 import { DB_GOOGLE_SHEET, SELECTED_DATABASE } from "@/config/constant";
 import Link from "next/link";
 import styles from "./Notes.module.css";
-import { google } from "googleapis";
 
 const getNotes = async () => {
   if (SELECTED_DATABASE === DB_GOOGLE_SHEET) {
-    if (true) {
-      const auth = await google.auth.getClient({
-        scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-      });
-      const googleSheet = google.sheets({ version: "v4", auth });
-      const range = `Sheet1!A:C`;
-      const res = await googleSheet.spreadsheets.values.get({
-        spreadsheetId: process.env.SHEET_ID,
-        range
-      });
-
-      const columnName = res.data.values?.shift();
-      let tempNotes;
-      if (columnName) {
-        tempNotes = res.data.values?.reduce((acc, el) => {
-          acc.push({ [columnName[0]]: el[0], [columnName[1]]: el[1], [columnName[2]]: el[2] });
-          return acc;
-        }, []);
-      }
-      return tempNotes;
-    } else {
-      // connect to google sheet through api (work on local but not work on vercel)
-      const res = await fetch("http://localhost:3219/api/google-sheet", {
-        cache: "no-store"
-      });
-      const data = await res.json();
-      return data as any[];
-    }
+    const res = await fetch("http://localhost:3219/api/google-sheet", {
+      cache: "no-store"
+    });
+    const data = await res.json();
+    return data as any[];
   }
 };
 
