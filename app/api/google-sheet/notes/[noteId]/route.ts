5aafb060d63dc419814d5d87eb5e85db5f1943ad
PrinceM <parames.pasitprachaja@gmail.com>
2023-04-03 12:49:17 +0700

    fix(console): remove console.log()

--- app/api/google-sheet/notes/[noteId]/route.ts
+++ app/api/google-sheet/notes/[noteId]/route.ts
@@ -4,7 +4,6 @@ export async function GET(request: Request, context: { params: any }) {
   const {
     params: { noteId }
   } = context;
-  console.log("params ? -----> ", noteId);
   const note = await getNote(+noteId);
   return new Response(JSON.stringify(note));
 }
